3ce5521b1b88e9284621991adc62e9868b5f97ec
Argir Argirov <dj.giro@gmail.com>
2017-11-11 17:42:09 -0500

    main page done

--- app/javascript/bundles/Animals/components/Animal.jsx
+++ app/javascript/bundles/Animals/components/Animal.jsx
@@ -1,22 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class Animals extends React.Component {
+export default class Animal extends React.Component {
   static propTypes = {
-    animals: PropTypes.array.isRequired, // this is passed from the Rails view
+    animal: PropTypes.obj.isRequired, // this is passed from the Rails view
   };
-
-  /**
-   * @param props - Comes from your rails view.
-   */
-  constructor(props) {
-    super(props);
-
-    // How to set initial state in ES6 class syntax
-    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { };
-  }
-
 
   render() {
 
